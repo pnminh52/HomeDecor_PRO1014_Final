12231375a9d241530e6ccffb3e5a611ab0c0759a
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-25 02:53:26 +0700

    Folder Fix

--- admin/index.php
+++ admin/index.php
@@ -1,32 +1,16 @@
 <?php
 session_start();
 require_once __DIR__ . "/../views/env.php";
-require_once __DIR__ . "/../common/function.php";
+require_once __DIR__ ."/../common/function.php";
+require_once __DIR__ ."/../models/BaseModel.php";
+require_once __DIR__ ."/../models/Category.php";
+require_once __DIR__ ."/../models/Product.php";
+require_once __DIR__ ."/../controllers/Admin/DashboardController.php";
+require_once __DIR__ ."/../controllers/Admin/AdminCategoryController.php";
 
-//include model
-require_once __DIR__ . "/../models/BaseModel.php";
-require_once __DIR__ . "/../models/Category.php";
-require_once __DIR__ . "/../models/Product.php";
 
-//include controller
-require_once __DIR__ . "/../controllers/Admin/AdminCategoryController.php";
-require_once __DIR__ . "/../controllers/admin/DashboardController.php";
-
-$ctl = $_GET['ctl'] ?? "";
-
-match ($ctl) {
-    "" => view("admin.dashboard"),
-    // "listsp" => (new AdminProductController)->index(),
-    // 'addsp' => (new AdminProductController)->create(),
-    // 'storesp' => (new AdminProductController)->store(),
-    // 'editsp' => (new AdminProductController)->edit(),
-    // 'updatesp' => (new AdminProductController)->update(),
-    // 'deletesp' => (new AdminProductController)->delete(),
-    // 'listdm' => (new AdminCategoryController)->index(),
-    // 'adddm' => (new AdminCategoryController)->add(),
-    // 'storedm' => (new AdminCategoryController)->store(),
-    // 'editdm' => (new AdminCategoryController)->edit(),
-    // 'updatedm' => (new AdminCategoryController)->update(),
-    // 'deletedm' => (new AdminCategoryController)->delete(),
-    default => view('404.404'),
+$ctl = $_GET['ctl'] ?? '';
+match ($ctl){
+    ''=>(new DashboardController)->index(),
 };
+
--- common/function.php
+++ common/function.php
@@ -1,12 +1,14 @@
 <?php
-//View function
-function view($path_view, $data=[]){
-    $path_view=str_replace(".","/", $path_view);
-    include_once ROOT_DIR . "views/path_view.php";
+
+function view($path_view, $data=[])
+{
+$path_view = str_replace(",","/", $path_view);
+include_once ROOT_DIR . "views/$path_view.php";
 }
-//Debug function
-function dd($data){
+
+function dd ($data)
+{
     echo "<pre>";
     var_dump($data);
-    echo "</pre>";
+     echo "</pre>";
 }
