5ef101d94eae25d9ce1828766ba56c0579509b48
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-25 06:04:55 +0700

    fix category, add product form

--- controllers/Admin/AdminProductController.php
+++ controllers/Admin/AdminProductController.php
@@ -6,20 +6,38 @@ class AdminProductController
     public function index()
 {
         $message = $_SESSION['message'] ?? '';
-        unset($_SESSION['message']); //xóa session
+        unset($_SESSION['message']); 
         $products = (new Product)->all();
         return view('admin.products.list', compact('products', 'message'));
     }
 
 
     public function add()
-    {}
+    {
+    $categories = (new Category)->all();
+    return view('admin.products.add', compact('categories'));
+}
+
+  
     
 
 
     public function store()
     {
-       
+        $data = $_POST;
+
+        $image = ''; //Trường hợp người dùng không nhập ảnh
+        $file = $_FILES['image'];
+        if ($file['size'] > 0) {
+            $image = $file['name'];
+            //Upload file
+            move_uploaded_file($file['tmp_name'], "../images/" . $image);
+        }
+        //thêm ảnh vào $data
+        $data['image'] = $image;
+        $product = new Product;
+        $product->create($data);
+        header("location: " . ADMIN_URL . "?ctl=listsp");
     }
 
 
---
+++ views/admin/products/add.php
@@ -0,0 +1,55 @@
+
+<?php include_once ROOT_DIR . "/admin/header.php" ?>
+<div class="conteiner">
+    <form action="<?= ADMIN_URL . '?ctl=storesp'?>" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="" class="form-label">Tên sản phẩm</label>
+            <input type="text" name="name" id="" class="form-control">
+        
+            
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Danh mục</label>
+        <select name="category_id" id="" class="form-control">
+            <?php foreach($categories as $cate):?>
+                <option value="<?=$cate['id']?>">
+                    <?=$cate['cate_name']?>
+                </option>
+                <?php endforeach?>
+        </select>
+            
+        </div>
+        <div class="mb-3">
+            <label for="">Hình ảnh</label>
+            <input type="file" name="image" id="" class="form-control">
+
+        </div>
+        <div class="mb-3">
+            <label for="">Giá</label>
+            <input type="number" name="price" id="" class="form-control">
+
+        </div>
+        <div class="mb-3">
+            <label for="">Số lượng</label>
+            <input type="number" name="quantity" id="" class="form-control">
+
+        </div>
+        <div class="mb-3">
+            <label for="">Trạng thái</label> <br>
+            <input type="radio" name="status" value="1" checked id=""> Đang kinh doanh
+            <input type="radio" name="status" value="0" id=""> Ngừng kinh doanh
+        </div>
+        <div class="mb-3">
+            <label for="">Mô tả sản phẩm</label>
+            <textarea name="description" class="form-control" rows="6"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="">Noi dung</label>
+            <textarea name="description" class="form-control" rows="8"></textarea>
+        </div>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Thêm mới</button>
+        </div>
+    </form>
+</div>
+<?php include_once ROOT_DIR . "/admin/footer.php" ?>
