ff1b8858794ede76de554288c6df4d7d1316c39e
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-25 02:34:25 +0700

    BaseModel

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,13 +1,13 @@
-<?php
-class BaseModel
-{
-    public $conn;
-    public function __construct()
-    {
-        try {
-            $this->conn = new PDO("mysql:host=localhost; dbname=homedecorfinal; charset=utf8", "root", "");
-        } catch (PDOException $e) {
-            echo "Lỗi kết nối cơ sở dữ liệu: " . $e->getMessage();
+<?php 
+
+class BaseModel {
+    public $conn=null;
+    public function __construct() {
+        try{
+$this->conn= new PDO("mysql:host=". HOST ."; dbname=" . DBNAME . "; charset=utf8; port=" .
+PORT, USERNAME, PASSWORD);
+        } catch(PDOException $e) {
+echo "Lỗi kết nối cơ sở dữ liệu:" . $e->getMessage();
         }
     }
 }
