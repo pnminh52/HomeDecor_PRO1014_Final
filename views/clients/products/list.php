eca29f16f1d94998447baed230125cd97e3836da
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-26 23:14:09 +0700

    list

--- views/clients/products/list.php
+++ views/clients/products/list.php
@@ -1,17 +1,17 @@
 <?php include_once ROOT_DIR . "/clients/header.php"?>
 <div class="container mt-5">
-<h2>Bàn gỗ</h2>
+<h2><?= $category_name?></h2>
 <div class="row g-4">
-    <?php foreach ($tables as $table): ?>
+    <?php foreach ($products as $product): ?>
     <div class="col-md-3">
         <div class="product-box">
-            <img src="<?= ROOT_URL .  $table['image']?>" alt="Product Image" class="product-img">
+            <img src="<?= ROOT_URL .  $product['image']?>" alt="Product Image" class="product-img">
             <div class="product-info">
                 <a href="#">
-                    <h5 class="product-name"><?= $table['name']?></h5>
+                    <h5 class="product-name"><?= $product['name']?></h5>
                 </a>
                 <div>
-                    <span class="product-price"><?= number_format($table['price'])?>đ</span>
+                    <span class="product-price"><?= number_format($product['price'])?>đ</span>
                 </div>
                 <div class="product-buttons">
 
