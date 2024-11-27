1aaa3401e33d1fff7dd78837d1822ece48a6eb92
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-27 10:02:19 +0700

    slideshow

--- views/clients/header.php
+++ views/clients/header.php
@@ -7,6 +7,7 @@
     <title>HomeDecor - <?= $title ??''?></title>
     <link rel="stylesheet" href="/views/template/css/common.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 </head>
 
--- views/clients/home.php
+++ views/clients/home.php
@@ -1,5 +1,39 @@
 <?php include_once ROOT_DIR . "views/clients/header.php"?>
 <div class="container mt-5">
+
+ <style>
+    .carousel-inner img {
+        height: 400px; 
+        object-fit: cover; 
+        width: 100%; 
+    }
+</style>
+
+ <div id="productSlideshow" class="carousel slide mb-5" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <?php 
+            $images = ['1.jpg', '2.jpg']; // Danh sách tên ảnh
+            foreach ($images as $index => $image): ?>
+            <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
+                <img src="<?= ROOT_URL . 'images/clients/' . $image ?>" class="d-block w-100" alt="Slideshow Image <?= $index + 1 ?>">
+               
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+                    <h5>HomeDecor</h5>
+                    <p>Khám phá các sản phẩm nổi bật.</p>
+                </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#productSlideshow" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#productSlideshow" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
 <h2>Bàn gỗ</h2>
 <div class="row g-4">
     <?php foreach ($tables as $table): ?>
