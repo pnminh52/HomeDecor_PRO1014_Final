d2c5dec9504bf50b0d60559f63a2b7a779d8175f
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-29 08:55:32 +0700

    ui update

--- views/clients/footer.php
+++ views/clients/footer.php
@@ -2,11 +2,70 @@
 <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-<footer class="text-white" style="background-color: rgb(255, 255, 255); padding-top: 30px;">
-    <div class="text-center py-3 bg-warning text-black">
-        <small>&copy; 2024 <strong>HomeDecor</strong>. All rights reserved.</small>
+<!-- Footer -->
+<!-- Footer -->
+<footer style="background-color: #eee6d3; width: 100%; margin-bottom: 0; padding-bottom: 0;">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4">
+        <h5 class="mb-3 text-dark">Homedecor - Nội thất sang trọng</h5>
+        <p>
+          Chúng tôi cung cấp những sản phẩm nội thất cao cấp, sang trọng và tiện nghi. 
+          Mang đến không gian sống hoàn hảo cho gia đình bạn.
+        </p>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h5 class="mb-3 text-dark">Liên kết</h5>
+        <ul class="list-unstyled mb-0">
+          <li class="mb-1">
+            <a href="#!" style="color: #4f4f4f;">Về chúng tôi</a>
+          </li>
+          <li class="mb-1">
+            <a href="#!" style="color: #4f4f4f;">Sản phẩm</a>
+          </li>
+          <li class="mb-1">
+            <a href="#!" style="color: #4f4f4f;">Dịch vụ</a>
+          </li>
+          <li>
+            <a href="#!" style="color: #4f4f4f;">Liên hệ</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h5 class="mb-1 text-dark">Giờ làm việc</h5>
+        <table class="table" style="border-color: #666;">
+          <tbody>
+            <tr>
+              <td>Thứ 2 - Thứ 6:</td>
+              <td>8:00 - 18:00</td>
+            </tr>
+            <tr>
+              <td>Thứ 7 - Chủ Nhật:</td>
+              <td>9:00 - 16:00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    &copy; 2024 Homedecor. Tất cả bản quyền được bảo lưu.
+  </div>
 </footer>
+<!-- End of Footer -->
+
+
+
+<style>
+  /* Loại bỏ khoảng trắng dưới footer */
+  footer {
+    margin-bottom: 0 !important;
+    padding-bottom: 0 !important;
+    margin-top: 30px !important;
+    max-width: 100%;
+  }
+</style>
+
 
 
 
