558ffc4f070d6484b0a91e24b4bd79f82c4e81e9
haivhph52554 <haivhph52554@gmail.com>
2024-11-29 21:20:38 +0700

    giỏ hàng

--- index.php
+++ index.php
@@ -15,16 +15,12 @@ $ctl=$_GET['ctl']??'';
 match ($ctl){
     ''=>(new HomeCotroller)->index(),
     'category' => (new ProductController)->list(),
-<<<<<<< HEAD
     'details' => (new ProductController)->show(),
     'add-cart'=>(new CartController)->addToCart(),
     'search' => (new ProductController)->searchs(),
     'view-cart'=>(new CartController)->viewCart(),
-
-
-=======
     'detail' => (new ProductController)->show(),
     'search'=> (new ProductController)->searchs(),
->>>>>>> b38846c17bbd0164d32fba846513539f415ad0b9
+
 
 };
