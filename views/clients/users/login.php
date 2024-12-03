6cc69548aa1b6cb1f04b24547a009730414a0fe7
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-12-03 11:13:40 +0700

    UI update

--- views/clients/users/login.php
+++ views/clients/users/login.php
@@ -27,12 +27,12 @@
 
             <div class="mb-3">
               <label for="loginPassword" class="form-label">Mật khẩu</label>
-              <input type="password" class="form-control" name="password" id="loginPassword" placeholder="Nhập email">
+              <input type="password" class="form-control" name="password" id="loginPassword" placeholder="Nhập mật khẩu">
             </div>
 
             
 
-            <button type="submit" class="btn btn-primary w-100">Đăng nhập</button>
+            <button type="submit" class="btn btnprimary w-100">Đăng nhập</button>
           <p>Bạn chưa có tài khoản? <a href="http://localhost/homedecorfinal/?ctl=register">Đăng ký</a> tại đây!
           </p>
           </form>
@@ -41,3 +41,47 @@
     </div>
 </div>
 
+<?php include_once ROOT_DIR . "views/clients/footer.php"; ?>
+<style>
+
+.alert {
+    margin-bottom: 20px;
+}
+
+.form-control {
+    padding: 10px;
+    font-size: 1rem;
+    border: 1px solid #ccc;
+}
+
+
+.btnprimary {
+    background-color:rgb(255, 255, 255);
+    border-color:rgb(0, 0, 0);
+    padding: 12px 20px;
+    color:black;
+    font-size: 1.1rem;
+    transition: background-color 0.3s ease, border-color 0.3s ease;
+    margin-bottom: 10px;
+}
+
+.btnprimary:hover {
+    background-color:rgb(0, 0, 0);
+    color:white;
+}
+
+p {
+    font-size: 0.9rem;
+    text-align: center;
+}
+
+p a {
+    color: #007bff;
+    text-decoration: none;
+}
+
+p a:hover {
+    text-decoration: underline;
+}
+
+</style>
--- views/clients/users/register.php
+++ views/clients/users/register.php
@@ -37,3 +37,46 @@
       </div>
     </div>
 </div>
+
+<?php include_once ROOT_DIR . "views/clients/footer.php"; ?>
+
+<style>
+    .alert {
+        margin-bottom: 20px;
+    }
+
+    .form-control {
+        padding: 10px;
+        font-size: 1rem;
+        border: 1px solid #ccc;
+    }
+
+    .btn-primary {
+        background-color: rgb(255, 255, 255);
+        border-color: rgb(0, 0, 0);
+        padding: 12px 20px;
+        color: black;
+        font-size: 1.1rem;
+        transition: background-color 0.3s ease, border-color 0.3s ease;
+        margin-bottom: 10px;
+    }
+
+    .btn-primary:hover {
+        background-color: rgb(0, 0, 0);
+        color: black;
+    }
+
+    p {
+        font-size: 0.9rem;
+        text-align: center;
+    }
+
+    p a {
+        color: #007bff;
+        text-decoration: none;
+    }
+
+    p a:hover {
+        text-decoration: underline;
+    }
+</style>
